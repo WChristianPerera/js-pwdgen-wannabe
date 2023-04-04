075b6630838c7ecb10323f334ba448fb3e90b549
WChristianPerera <christianperera13@gmail.com>
2023-04-04 18:02:04 +0200

    Todofix

--- js/script.js
+++ js/script.js
@@ -5,4 +5,9 @@ const userSurname = prompt (`Qual'è il tuo cognome ?`);
 document.getElementById('u-surname').innerHTML = userSurname;
 
 const favouriteColour = prompt (`Qual'è il tuo colore preferito ?`);
-document.getElementById('color').innerHTML = favouriteColor;
+document.getElementById('color').innerHTML = favouriteColour;
+
+
+const generatePassword = userName + userSurname + favouriteColour;
+const createPassword = generatePassword;
+document.getElementsById('password').innerHTML = createPassword;
