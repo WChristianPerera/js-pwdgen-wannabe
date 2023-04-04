edfa014f1dfb586b18ac06fc7357d5e133fa2400
WChristianPerera <christianperera13@gmail.com>
2023-04-04 17:21:14 +0200

    Add surname and favourite color

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
-const userName = prompt ('Come ti chiami ?')
-document.getElementById('u-name').innerHTML = userName
+const userName = prompt ('Come ti chiami ?');
+document.getElementById('u-name').innerHTML = userName;
 
-const userSurname = prompt ()
+const userSurname = prompt (`Qual'è il tuo cognome ?`);
+document.getElementById('u-surname').innerHTML = userSurname;
+
+const favouriteColour = prompt (`Qual'è il tuo colore preferito ?`);
+document.getElementById('color').innerHTML = favouriteColor;
